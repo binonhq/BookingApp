42327ac10adb3e09c4bb4c844387b9689f4f1ae4
Quan MN <quankhanhvan3@gmail.com>
2023-06-23 00:10:59 +0700

    user

--- back-end/server.js
+++ back-end/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require('cors')
 require("dotenv").config();
 const app = express();
-const connectDB = require("./src/config/mongo");
+const connectDB = require("./config/mongo");
 
 const PORT = process.env.PORT;
 app.use(cors())
@@ -14,12 +14,12 @@ app.use(
 );
 
 // import routers
-const userRouter = require("./src/routes/user");
-const productRouter = require("./src/routes/product");
+const userRouter = require("./routes/user");
+// const productRouter = require("./routes/product");
 
 // use routers
 app.use("/api/user", userRouter);
-app.use("/api/products", productRouter);
+// app.use("/api/products", productRouter);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
